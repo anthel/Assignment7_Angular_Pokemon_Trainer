22a7d00331bf9f737de761de8d7c01f12bdc686b
Ante <ante.hellgren@se.experis.com>
2021-08-26 15:08:21 +0200

    background gradient and colors

--- pokemon-trainer/src/app/login-page/login-page.component.ts
+++ pokemon-trainer/src/app/login-page/login-page.component.ts
@@ -8,7 +8,7 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
 
